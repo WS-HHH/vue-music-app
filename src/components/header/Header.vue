13f80a4b260788773b32237fc3ab6c7af77d109c
shuang <1395363307@qq.com>
2021-08-11 23:04:05 +0800

    fix(vue.config.js): config global scss

---
+++ src/components/header/Header.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="header">
+    <span class="icon"></span>
+    <h1 class="text">Chicken Music</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+<style lang="scss" scoped>
+  #header {
+    height: 44px;
+  }
+</style>
