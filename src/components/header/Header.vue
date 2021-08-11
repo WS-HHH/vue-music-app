14639b4be294aad948bc5d61bb3311931236b1e1
shuang <1395363307@qq.com>
2021-08-11 23:31:32 +0800

    feat(src): add header component

--- src/components/header/Header.vue
+++ src/components/header/Header.vue
@@ -14,5 +14,25 @@ export default {
 <style lang="scss" scoped>
   #header {
     height: 44px;
+    text-align: center;
+    color: $color-theme;
+    font-size: 0;
+    .icon {
+      display: inline-block;
+      vertical-align: top;
+      margin-top: 6px;
+      margin-right: 9px;
+      // 实现图片缩放
+      width: 30px;
+      height: 32px;
+      @include bg-image('logo');
+      background-size: 30px 32px;
+    }
+    .text {
+      display: inline-block;
+      line-height: 44px;
+      //vertical-align: top;
+      font-size: $font-size-large;
+    }
   }
 </style>
